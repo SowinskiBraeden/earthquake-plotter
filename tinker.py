ebacf1c5ae1bd2cdc80b2eae2afc4826fee3b932
Braeden Sowinski <sowinskibraeden@gmail.com>
2022-05-22 21:12:38 -0700

    test commit

--- tinker.py
+++ tinker.py
@@ -7,7 +7,7 @@ from mpl_toolkits.basemap import Basemap
 eq_lat, eq_lon = [], []
 magnitudes, eq_ts = [], []
 
-# test commit 2
+# test commit
 
 with open('2.5_week.csv', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
